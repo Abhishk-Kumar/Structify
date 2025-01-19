4d6753087a2871ba881b4257dc6f0cf14c1006b4
Abhishk Kumar <abhishkdk@gmail.com>
2025-01-19 19:14:24 +0530

    updation

--- script.js
+++ script.js
@@ -4,12 +4,31 @@
 //     });
 // });
 
+// const menuIcon = document.querySelector('.menu-icon');
+// const menu = document.querySelector('.menu');
+
+// menuIcon.addEventListener('click', () => {
+//   menu.classList.toggle('show');
+// });
+
+
+
+
+// script.js
 const menuIcon = document.querySelector('.menu-icon');
 const menu = document.querySelector('.menu');
+const hamburgerIcon = document.querySelector('.ham-button');
+const overlay = document.getElementById('overlay');
 
 menuIcon.addEventListener('click', () => {
   menu.classList.toggle('show');
 });
+
+hamburgerIcon.addEventListener('click', () => {
+  overlay.style.display = overlay.style.display === 'block' ? 'none' : 'block';
+});
+
+
 
 
 
